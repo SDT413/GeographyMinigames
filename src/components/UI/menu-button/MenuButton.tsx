642c6b031744dabbc2b760c0d563dd09f5ecb840
MKO <kiril309274@gmail.com>
2024-05-29 15:47:52 +0200

    custom styles fix

--- src/components/UI/menu-button/MenuButton.tsx
+++ src/components/UI/menu-button/MenuButton.tsx
@@ -67,29 +67,28 @@ const MenuButton: FC<PropsWithChildren<Props>> = ({children, addStyles, onClick,
     return (
         <>
             { link && selected ?
-            <Link href={link} className={classNames(styles.button, addStyles, styles.selected)}>
-                <button
+                <Link href={link} className={classNames(styles.button, addStyles, styles.selected)}>
+                    <button
                         onClick={onClick}>
-                    {children}
+                        {children}
+                    </button>
                     <svg width={starWidth} height={starHeight} viewBox="0 0 152 141" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M76 0L93.7366 53.5512L151.133 53.5512L104.698 86.6476L122.435 140.199L76 107.102L29.565 140.199L47.3016 86.6476L0.866539 53.5512L58.2634 53.5512L76 0Z"
                               fill="#D9D9D9"/>
                     </svg>
-
-                </button>
-            </Link>
-            :
-            link && !selected ?
-            <Link href={link} className={classNames(styles.button, addStyles)}>
-            <button
-                    onClick={onClick}>
-                {children}
-            </button>
-        </Link>
-        :
-        selected ?
-                <button className={classNames(styles.button, addStyles, styles.selected)}
-                        onClick={onClick}>
+                </Link>
+                :
+                link && !selected ?
+                    <Link href={link} className={classNames(styles.button, addStyles)}>
+                        <button
+                            onClick={onClick}>
+                            {children}
+                        </button>
+                    </Link>
+                    :
+                    selected ?
+                        <button className={classNames(styles.button, addStyles, styles.selected)}
+                                onClick={onClick}>
                     {children}
                     <svg width={starWidth} height={starHeight} viewBox="0 0 152 141" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M76 0L93.7366 53.5512L151.133 53.5512L104.698 86.6476L122.435 140.199L76 107.102L29.565 140.199L47.3016 86.6476L0.866539 53.5512L58.2634 53.5512L76 0Z"
